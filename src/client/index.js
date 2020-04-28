70cc1fd210bfdded471555342634331c929608c1
Sharon <paradesi@amazon.com>
2020-04-27 22:52:28 -0400

    Make client and server functional
    Changes:
    - Pattern of making external call, saving results, and updating UI
    - Required changes needed

--- src/client/index.js
+++ src/client/index.js
@@ -7,28 +7,52 @@ import './styles/form.scss'
 import './styles/header.scss'
 import './styles/resets.scss'
 
-//console.log(checkForName);
-//console.log(handleSubmit);
-
-function logSubmit(event) {
-//   log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-   event.preventDefault();
-   console.log('in logSubmit');
-   let formText = document.getElementById('name').value;
-   console.log('formText: ' + formText);
-
-   postData('/sentiment', {text: formText});
+const formInputValidationReasonCode = {
+    EMPTY: 'empty',
+    URL_PARSE_ERROR: 'url_parse_error',
+    OK: 'ok',
 }
 
-// Using event listener per mentor advice at https://knowledge.udacity.com/questions/82043
-// Accepts both keypress and button clicks.
-const form = document.getElementById('form');
-form.addEventListener('submit', logSubmit);
+function validateFormInput(url) {
+  // Pattern: http|https:www.example.com
+  // TODO: Test
+  const pattern = new RegExp('^https?:\/\/[a-zA-Z0-9]+.[a-zA-Z0-9]+.[a-zA-Z0-9]+\/[a-zA-Z0-9-]+$');
+
+  if (url.length <= 0) {
+    console.log("Empty input provided. Please enter a URL");
+    return formInputValidationReasonCode.EMPTY;
+  }
+
+  const isUrlPatternMatched = pattern.test(url);
+  if(!isUrlPatternMatched) {
+    return formInputValidationReasonCode.URL_PARSE_ERROR;
+  }
+
+  return formInputValidationReasonCode.OK;
+}
+
+// Call local server to post the results
+const getSummary = async(localUrl, apiUrl) => {
+  const request = await fetch(localUrl, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(apiUrl)
+  });
+
+  try {
+    const response = await request.json();
+
+    return response;
+  } catch(error) {
+    console.log("Error: ", error);
+  }
+}
 
 // Call local server to post the results
 const postData = async(url, data) => {
-  console.log('url: ' + url);
-  console.log('data: ' + JSON.stringify(data));
   const response = await fetch(url, {
     method: 'POST',
     credentials: 'same-origin',
@@ -47,3 +71,41 @@ const postData = async(url, data) => {
   }
 }
 
+// Call local server to get the results and render on page
+const updateUI = async() => {
+  try {
+    const request = await fetch('/all');
+    const response = await request.json();
+
+    document.getElementById('results').innerHTML = 'Summary: ' + response.sentences;
+  } catch(error) {
+    console.log("Error: ", error);
+  }
+}
+
+// Button handler that chains a series of Promises (async calls) using `then`
+function performAction(event) {
+  event.preventDefault();
+  const url = document.getElementById('name').value;
+
+  const validationResult = validateFormInput(url);
+  if (!validationResult) {
+    // Update form to indicate error
+    console.log('validation failed');
+  } else {
+    getSummary('/summarize', {url: url})
+    .then(function(data) {
+        const sentencesCollection = data.sentences;
+        const sentences = sentencesCollection.join(' ');
+        postData('/add', {sentences: sentences});
+    })
+    .then(function() {
+      return updateUI();
+    });
+  }
+}
+
+// Using event listener per mentor advice at https://knowledge.udacity.com/questions/82043
+// Accepts both keypress and button clicks.
+const form = document.getElementById('form');
+form.addEventListener('submit', performAction);
--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Setup empty JS object to act as endpoint for all routes
+let projectData = {};
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
-
 app.use(express.static('dist'))
-
-console.log(__dirname)
 
 var aylien = require("aylien_textapi");
 
@@ -19,27 +19,13 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-console.log(`Your API key is ${process.env.API_KEY}`);
 const textapi = new aylien({
-application_id: process.env.API_ID,
-application_key: process.env.API_KEY
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
 });
-
-/*
-// Example from https://docs.aylien.com/textapi/sdks/#node-js-sdk
-const result = textapi.sentiment({
-  'text': 'John is a very good football player!'
-}, function(error, response) {
-  if (error === null) {
-    console.log(response);
-  }
-});
-console.log('result: ' + result);
-*/
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+  res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -51,22 +37,35 @@ app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
 
-// POST route
-app.post('/sentiment', callAylien);
-function callAylien(request, response) {
-  console.log('inside callMyAlient');
-  const body = request.body;
-  console.log('request.body: ' + JSON.stringify(body));
-  console.log('text: ' + body.text);
+// GET route
+app.get('/all', sendData);
+function sendData(request, response) {
+  response.send(projectData);
+}
 
-  const result = textapi.sentiment({
-    'text': body.text
-  }, function(error, response) {
+// POST route
+app.post('/add', addSummaryData);
+function addSummaryData(request, response) {
+  const body = request.body;
+  projectData = {sentences: body.sentences};
+}
+
+// POST route
+app.post('/summarize', callAylien);
+function callAylien(request, response) {
+  const body = request.body;
+//  const result = "abc";
+
+  textapi.summarize({
+    url: body.url,
+    sentences_number: 3
+  }, function(error, responseSummary) {
     if (error === null) {
-      console.log(response);
+      response.send(responseSummary);
+    } else {
+      console.log("ERROR"); // To show on UI
     }
   });
-  console.log('result: ' + result);
 }
 
 module.exports = server
